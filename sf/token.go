3206f04bee1c41c6a65d81c61c8080348d4e49f3
renthraysk <renthraysk@gmail.com>
2025-11-10 12:52:45 +0000

    token is tchar | ':' | '/'

--- sf/token.go
+++ sf/token.go
@@ -9,9 +9,11 @@ func isTokenStart(c byte) bool {
 func isToken(c byte) bool {
 	const tchar = digit | upper | lower |
 		1<<'!' | 1<<'#' | 1<<'$' | 1<<'%' | 1<<'&' | 1<<'\'' | 1<<'*' |
-		1<<'+' | 1<<'-' | 1<<'.' | 1<<'^' | 1<<'_' | 1<<'`' | 1<<'|' | 1<<'~'
+		1<<'+' | 1<<'-' | 1<<'.' | 1<<'^' | 1<<'_' | 1<<'`' | 1<<'|' | 1<<'~' |
+		1<<':' | 1<<'/'
+	const token = tchar | 1<<':' | 1<<'/'
 
-	return isASCII(c, tchar%(1<<64), tchar>>64)
+	return isASCII(c, token%(1<<64), token>>64)
 }
 
 func tokenCut(s string) (string, string, bool) {
