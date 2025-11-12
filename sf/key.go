83435a459f901fb2609e8e3082a3240f49db9a5c
renthraysk <renthraysk@gmail.com>
2025-11-12 17:24:55 +0000

    key cleanup

--- sf/key.go
+++ sf/key.go
@@ -2,16 +2,12 @@ package sf
 
 import "slices"
 
-const keySet = lower | digit | 1<<'_' | 1<<'-' | 1<<'.' | 1<<'*'
-
-func isKey(c byte) bool { return isASCII(c, keySet%(1<<64), keySet>>64) }
-
-func isKeyStart(c byte) bool { return isLower(c) || c == '*' }
-
 // https://www.rfc-editor.org/rfc/rfc8941.html#name-serializing-a-key
 func KeyValid(key string) bool {
+	const keySet = lower | digit | 1<<'_' | 1<<'-' | 1<<'.' | 1<<'*'
+
 	return len(key) > 0 &&
-		isKeyStart(key[0]) &&
+		(isLower(key[0]) || key[0] == '*') &&
 		isASCIIValid(key[1:], keySet%(1<<64), keySet>>64)
 }
 
